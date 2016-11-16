1824be81d6d7d46e1e50a27f63d0c65fe0cf56b2
test <esgfsf@wefwfe.com>
2016-11-16 21:52:42 +0100

    Used MVC and Bootstrap

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("", "No AOL asshole");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAdress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// site.js
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("David Fiebig");
+
+    //var main = $("#main");
+    //main.mouseenter(function() {
+    //    main.attr("style", "background-color: #888;");
+    //});
+
+    //main.mouseleave(function() {
+    //    main.attr("style", "");
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert("Hello " + me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
+
+    $("#sidebarToggle").on("click", function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        } else {
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
+        }
+    });
+
+})();
